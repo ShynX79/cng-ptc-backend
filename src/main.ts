e1d0d95deb12c1f894191ba8fe372eaebfbe269b
ShynX79 <mtotikurniawan@gmail.com>
2025-09-02 17:43:40 +0700

    fix : deployment menggunakan render

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,57 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'ngrok-skip-browser-warning',
+    ],
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
     .setTitle('CNG PTC API')
-    .setDescription('API documentation for CNG PTC')
+    .setDescription('Dokumentasi lengkap untuk backend aplikasi CNG PTC.')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Masukkan token JWT',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document); // <= ini yang bikin /docs jalan
+  SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  // 👇 Tambahin ini biar Vercel bisa serve request sebelum listen
+  await app.init();
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Aplikasi berjalan di: ${await app.getUrl()}`);
+  console.log(
+    `Dokumentasi Swagger tersedia di: ${await app.getUrl()}/api-docs`,
+  );
 }
 bootstrap();
+
