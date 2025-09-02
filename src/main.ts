03bec731b71b8ea2a1e9b1d7a4a889e16da0925a
ShynX79 <mtotikurniawan@gmail.com>
2025-09-02 16:24:54 +0700

    fix : deplloy pt 20

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +13,13 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  // Gunakan swagger-ui-express
-  app.use('/api', swaggerUi.serve, swaggerUi.setup(document));
+  // Swagger UI di /api
+  SwaggerModule.setup('api', app, document);
+
+  // Swagger JSON manual di /api-json
+  app.getHttpAdapter().getInstance().get('/api-json', (req, res) => {
+    res.json(document);
+  });
 
   await app.listen(3000);
 }
